b228cb0a44bca465642c24243cd9aa658e2b79c1
hsinnosuketw <hsinnosuketw@gmail.com>
2025-03-28 08:33:32 +0000

    Update embeddings.py
    Comment out out-dated
    get_embedding_function().
    Replace it with new
    embedding function "infly
    inf-retriever-v1-1.5b".

--- src/medllm/core/embeddings.py
+++ src/medllm/core/embeddings.py
@@ -1,12 +1,12 @@
 from langchain_community.embeddings import GPT4AllEmbeddings
 from ..config.config import MODEL_NAME_EMBED, GPT4ALL_KWARGS
 
-def get_embedding_function():
-    return GPT4AllEmbeddings(
-        model_name=MODEL_NAME_EMBED,
-        gpt4all_kwargs=GPT4ALL_KWARGS,
-        # device="cuda",
-    )
+# def get_embedding_function():
+#     return GPT4AllEmbeddings(
+#         model_name=MODEL_NAME_EMBED,
+#         gpt4all_kwargs=GPT4ALL_KWARGS,
+#         # device="cuda",
+#     )
 
 # for new vectorstore
 
