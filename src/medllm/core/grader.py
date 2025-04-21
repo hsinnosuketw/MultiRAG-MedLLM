224e3bede1f83b113a7f1625eae8175de9b214ba
LilbonMage <lilbonmage335@gmail.com>
2025-04-21 09:45:32 +0000

    chore: remove unused functions
    Remove the grade_retrieval and filter_retrieval functions which were deprecated.

--- src/medllm/core/grader.py
+++ src/medllm/core/grader.py
@@ -5,30 +5,27 @@ from src.medllm.core.prompt import AnswerGenerationPrompt
 from src.medllm.config.config import MODEL_ID_RETRIEVER, API_KEY
 
 def get_retrieval_llm():
-    return ChatNVIDIA(model=MODEL_ID_FILTER, temperature=0)
+    """
+    Initialize the NVIDIA retrieval model.
 
-def grade_retrieval(question, documents):
-    prompt = PromptTemplate(
-        template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing relevance
-        of a retrieved document to a user question. If the document contains keywords related to the user question or is a deterministic answer to the user question,
-        grade it as relevant. Give a binary score "yes" or "no" in JSON with key "score".
-        EXAMPLE: {"score" : "yes"} or {"score" : "no"}
-        <|eot_id|><|start_header_id|>user<|end_header_id>
-        Document: {document}\nQuestion: {question} <|eot_id|>""",
-        input_variables=["question", "document"]
-    )
-    llm = get_retrieval_llm()
-    retrieval_grader = prompt | llm | JsonOutputParser()
-    return retrieval_grader.invoke({"question": question, "document": documents})
+    This function sets the temperature to 0 for deterministic results.
 
-def filter_retrieval(question, documents):
-    prompt = PromptTemplate(template=RetrieverFilterPrompt, input_variables=["question", "documents"])
-    llm = get_retrieval_llm()
-    retrieval_filter = prompt | llm | JsonOutputParser()
-    filtered = retrieval_filter.invoke({"question": question, "documents": documents})
-    return [Document(page_content=f["page_content"]) for f in filtered['filtered docs']]
+    Returns:
+        ChatNVIDIA: An instance of the ChatNVIDIA class configured for retrieval.
+    """
+    return ChatNVIDIA(model=MODEL_ID_RETRIEVER, temperature=0)
 
 def rank_documents(question, documents):
+    """
+    Rank documents using NVIDIA's reranking model.
+
+    Args:
+        question (str): The query string.
+        documents (list): List of Document objects to be ranked.
+
+    Returns:
+        list: List of Document objects sorted by relevance score.
+    """
     ranker = NVIDIARerank(model="nv-rerank-qa-mistral-4b:1", api_key=API_KEY)
     ranker.top_n = 5
     threshold = 0
@@ -36,6 +33,17 @@ def rank_documents(question, documents):
     return [doc for doc in reranked_docs if doc.metadata.get("relevance_score", 0) > threshold]
 
 def generate_answer(question, context):
+    """
+    Generate an answer to a question using answer generator.
+
+    Args:
+        question (str): The question to be answered.
+        context (list): List of Document objects containing the context.
+
+    Returns:
+        str: The generated answer.
+    """
+
     prompt = PromptTemplate(template=AnswerGenerationPrompt, input_variables=["question", "context"])
     llm = get_retrieval_llm()
     rag_chain = prompt | llm | StrOutputParser()
